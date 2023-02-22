f166924fd675a97962f937bbe1325cea83af09c6
Miguel <99726342+mig-code@users.noreply.github.com>
2023-02-22 19:25:32 +0100

    :construction: Add mock productDetails view

---
+++ src/features/product.details/components/details.card/details.card.jsx
@@ -0,0 +1,106 @@
+import { Link } from 'react-router-dom';
+
+export function DetailsCards({ mobileData }) {
+    return (
+        <div className="details-container">
+            <div className="details-left">
+                <img
+                    className="details-left__image"
+                    src={mobileData.imgUrl}
+                    alt={mobileData.model}
+                />
+            </div>
+            <div className="details-right">
+                <div className="details-right__properties-box">
+                    <h1 className="details-right__model">
+                        {mobileData.model}{' '}
+                    </h1>
+                    <div className="details-right__feature">
+                        Marca:
+                        <span className="details-right--textmodifier">
+                            {mobileData.brand}
+                        </span>
+                    </div>
+
+                    <p className="details-right__feature">
+                        PRECIO:
+                        <span className="details-right--textmodifier">
+                            {mobileData.price} €
+                        </span>
+                    </p>
+                    <p className="details-right__feature">
+                        RAM:
+                        <span className="details-right--textmodifier">
+                            {mobileData.ram}
+                        </span>
+                    </p>
+                    <p className="details-right__feature">
+                        OS:
+                        <span className="details-right--textmodifier">
+                            {mobileData.os} €
+                        </span>
+                    </p>
+                    <p className="details-right__feature">
+                        Resolución:
+                        <span className="details-right--textmodifier">
+                            {mobileData.displayResolution}
+                        </span>
+                    </p>
+                    <p className="details-right__feature">
+                        Batería:
+                        <span className="details-right--textmodifier">
+                            {mobileData.battery}
+                        </span>
+                    </p>
+                    <p className="details-right__feature">
+                        Cámara Principal:
+                        <span className="details-right--textmodifier">
+                            {mobileData.primaryCamera[0]}
+                        </span>
+                    </p>
+                    <p className="details-right__feature">
+                        Cámara Secundaria:
+                        <span className="details-right--textmodifier">
+                            {mobileData.secondaryCmera}
+                        </span>
+                    </p>
+                    <p className="details-right__feature">
+                        Tamaño:
+                        <span className="details-right--textmodifier">
+                            {mobileData.dimentions}
+                        </span>
+                    </p>
+                    <p className="details-right__feature">
+                        Peso:
+                        <span className="details-right--textmodifier">
+                            {mobileData.weight} g
+                        </span>
+                    </p>
+                </div>
+
+                <div className="actions">
+                    <div className="actions__colors-box">
+                        {mobileData.options.colors.map((item, index) => (
+                            <div key={item.code}>{item.name}</div>
+                        ))}
+                    </div>
+                    <div className="actions__storage-box">
+                        {mobileData.options.storages.map((item, index) => (
+                            <div key={item.code}>{item.name}</div>
+                        ))}
+                    </div>
+                    <div className="actions__buttons-box">
+                        <Link to={`/`}>
+                            <button className="actions__button actions__button--back">
+                                VOLVER
+                            </button>
+                        </Link>
+                        <button className="actions__button actions__button--add">
+                            AÑADIR AL CARRITO
+                        </button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
--- src/features/product.details/pages/product.details.page.jsx
+++ src/features/product.details/pages/product.details.page.jsx
@@ -1,7 +1,19 @@
+import { useEffect, useState } from 'react';
+import { getMobileDetailsMock } from '../../../mocks/mobiles';
+import { DetailsCards } from '../components/details.card/details.card';
 export default function ProductDetailsPage() {
+    const [mobileData, setMobileData] = useState({});
+
+    useEffect(() => {
+        setMobileData(getMobileDetailsMock());
+    }, []);
+
     return (
         <div>
             <h2>Product Details Page</h2>
+            {mobileData.id && (
+                <DetailsCards mobileData={mobileData}></DetailsCards>
+            )}
         </div>
     );
 }
