59f66dccbb7a2e3751e4a545249d25ee2d295674
Miguel <99726342+mig-code@users.noreply.github.com>
2023-02-22 18:42:32 +0100

    :construction: add mock ProductsList view

---
+++ src/features/products.list/components/item/item.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export function Item({ item }) {
+    return (
+        <Link to={`/mobile/${item.id}`}>
+            <div>
+                <img
+                    className="mobile-box__image"
+                    src={item.imgUrl}
+                    alt={item.model}
+                />
+            </div>
+
+            <div>
+                <p className="mobile-box__brand">{item.brand} </p>
+                <p className="mobile-box__model"> {item.model}</p>
+            </div>
+            <p className="mobile-box__price">{item.price} €</p>
+        </Link>
+    );
+}
