aa36a59c14d6f7341f767b848eaa5a8bbde34b04
Miguel <99726342+mig-code@users.noreply.github.com>
2023-02-23 12:30:15 +0100

    :construction: Update pages loading from api

--- src/features/products.list/pages/products.list.page.jsx
+++ src/features/products.list/pages/products.list.page.jsx
@@ -1,14 +1,29 @@
-import { useEffect, useState } from 'react';
-import { getAllMobilesMock } from '../../../mocks/mobiles';
+import { useEffect, useState, useCallback } from 'react';
+
 import { List } from '../components/list/list';
 import { SearchBox } from '../components/search.box/search.box';
+import { getProducts } from '../../../core/services/products.services';
+import { consoleDebug } from '../../../tools/debug';
 
 export default function ProductsListPage() {
     const [products, setProducts] = useState([]);
+    const handleError = (error) => {
+        consoleDebug(error);
+    };
+
+    const handleLoadProducts = useCallback(async () => {
+        try {
+            const products = await getProducts();
+            setProducts(products);
+        } catch (error) {
+            handleError(error);
+        }
+    }, []);
 
     useEffect(() => {
-        setProducts(getAllMobilesMock());
-    }, []);
+        console.log('ProductsListPage useEffect');
+        handleLoadProducts();
+    }, [handleLoadProducts]);
 
     return (
         <div>
