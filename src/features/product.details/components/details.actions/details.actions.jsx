2f27609d9d50b28fba2db0c9cecbdf8c9c897a07
Miguel <99726342+mig-code@users.noreply.github.com>
2023-02-25 11:11:08 +0100

    :construction: Add mobileForm to DetailsAction

--- src/features/product.details/components/details.actions/details.actions.jsx
+++ src/features/product.details/components/details.actions/details.actions.jsx
@@ -1,18 +1,52 @@
 import { Link } from 'react-router-dom';
-
+import { useState } from 'react';
 
 export function DetailsActions({ mobileData }) {
+    const initialMobileFormState = {
+        id: mobileData.id,
+        colorCode: mobileData.options.colors[0].code,
+        storageCode: mobileData.options.storages[0].code,
+    };
+
+    const [mobileForm, setMobileForm] = useState(initialMobileFormState);
+
+    const handleMobileFormInput = (event) => {
+        const { name, value } = event.target;
+        setMobileForm({ ...mobileForm, [name]: parseInt(value, 10) });
+        
+    };
+
+    const handleSubmitMobileForm = (event) => {
+        event.preventDefault();
+        console.log(mobileForm);
+    };
+
     return (
-        <div className="actions">
+        <form className="actions" onSubmit={handleSubmitMobileForm}>
             <div className="actions__colors-box">
-                {mobileData.options.colors.map((item, index) => (
-                    <div key={item.code}>{item.name}</div>
-                ))}
-            </div>
-            <div className="actions__storage-box">
-                {mobileData.options.storages.map((item, index) => (
-                    <div key={item.code}>{item.name}</div>
-                ))}
+                <select
+                    name="colorCode"
+                    value={mobileForm.colorCode}
+                    onChange={handleMobileFormInput}
+                >
+                    {mobileData.options.colors.map((item, index) => (
+                        <option key={item.code} value={item.code}>
+                            {item.name}
+                        </option>
+                    ))}
+                </select>
+
+                <select
+                    name="storageCode"
+                    value={mobileForm.storageCode}
+                    onChange={handleMobileFormInput}
+                >
+                    {mobileData.options.storages.map((item, index) => (
+                        <option key={item.code} value={item.code}>
+                            {item.name}
+                        </option>
+                    ))}
+                </select>
             </div>
             <div className="actions__buttons-box">
                 <Link to={`/`}>
@@ -20,10 +54,13 @@ export function DetailsActions({ mobileData }) {
                         VOLVER
                     </button>
                 </Link>
-                <button className="actions__button actions__button--add">
+                <button
+                    type="submit"
+                    className="actions__button actions__button--add"
+                >
                     AÑADIR AL CARRITO
                 </button>
             </div>
-        </div>
+        </form>
     );
 }
