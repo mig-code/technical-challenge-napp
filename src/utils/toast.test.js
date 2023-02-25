bf5d6d2a2f92c54e72432e89e10f3f56da9d383f
Miguel <99726342+mig-code@users.noreply.github.com>
2023-02-25 23:36:01 +0100

    :white_check_mark: Add toast and useCart test

---
+++ src/utils/toast.test.js
@@ -0,0 +1,33 @@
+import { toast } from 'react-hot-toast';
+
+import { notifySuccess, notifyError } from './toasts';
+
+jest.mock('react-hot-toast');
+
+describe('Given Toast Module', () => {
+    describe('When notifySuccess function is called', () => {
+        test('Then toast.success should be called', () => {
+            notifySuccess();
+            expect(toast.success).toHaveBeenCalled();
+        });
+    });
+
+    describe('When notifyError function is called', () => {
+        const mockStyles = {
+            duration: 1500,
+            style: {
+                border: '1px solid #ec5431',
+                padding: '16px',
+                color: '#ec5431',
+            },
+            iconTheme: {
+                primary: '#ec5431',
+                secondary: '#fffaee',
+            },
+        };
+        test('Then toast.error should be called', () => {
+            notifyError('Error');
+            expect(toast.error).toHaveBeenCalledWith('Error', mockStyles);
+        });
+    });
+});
