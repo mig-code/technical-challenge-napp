ee75f6974f970a1559ca1959eb083615deb98b72
Miguel <99726342+mig-code@users.noreply.github.com>
2023-02-28 12:32:41 +0100

    :adhesive_bandage: fix sonar code smell

--- src/utils/search.filters.js
+++ src/utils/search.filters.js
@@ -1,7 +1,5 @@
 export const filterByModelAndBrand = (products, query) => {
-    let filteredProducts = products;
-
-    filteredProducts = products.filter((product) => {
+    const filteredProducts = products.filter((product) => {
         return (
             product.model.toLowerCase().includes(query.toLowerCase()) ||
             product.brand.toLowerCase().includes(query.toLowerCase())
