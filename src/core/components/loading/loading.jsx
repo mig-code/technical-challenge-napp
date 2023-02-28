129787fd962d2725add4539e0f7a6277092a9118
Miguel <99726342+mig-code@users.noreply.github.com>
2023-02-28 11:07:15 +0100

    :lipstick: Add favicon a fix spinner

--- src/core/components/loading/loading.jsx
+++ src/core/components/loading/loading.jsx
@@ -1,10 +1,9 @@
 import './loading.scss';
 
 export function Loading() {
-    console.log('RENDER: Loading');
     return (
         <div className="loading">
-            <div class="spinner"></div>
+            <div className="spinner"></div>
         </div>
     );
 }
