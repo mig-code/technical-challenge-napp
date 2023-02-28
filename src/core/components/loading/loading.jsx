e89583d20c9ccfbba596d9a5a4c69faec633b98b
Miguel <99726342+mig-code@users.noreply.github.com>
2023-02-28 10:56:28 +0100

    :lipstick: Add spinner

--- src/core/components/loading/loading.jsx
+++ src/core/components/loading/loading.jsx
@@ -1,8 +1,10 @@
+import './loading.scss';
+
 export function Loading() {
     console.log('RENDER: Loading');
     return (
         <div className="loading">
-            <div className="loading__spinner">LOADING</div>
+            <div class="spinner"></div>
         </div>
     );
 }
